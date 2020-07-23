01467c710000c4734a4c0b8138a0d48ebb3dd185
auayson <67250208+auayson@users.noreply.github.com>
2020-07-23 17:03:52 +0800

    changed the patients migration schema

---
+++ database/migrations/2020_07_23_092600_create_patient_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            
+            $table->id();
+            $table->integer('internal_id')->nullable();
+            $table->string('patient_name',100)->nullable();
+            $table->string('address',100)->nullable();
+            $table->string('dob',100)->nullable();
+            $table->string('sex',100)->nullable();
+            $table->string('ethnicity',100)->nullable();
+            $table->string('home_phone',100)->nullable();
+            $table->string('work_phone',100)->nullable();
+            $table->string('mobile_phone',100)->nullable();
+            $table->string('email',100)->nullable();
+            $table->string('record_status',100)->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
+
+
+
+
+
+
+
+
+
+
